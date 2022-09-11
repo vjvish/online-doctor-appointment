b0ffe9322cbb52c5ca5d1343fe543c8edf14d8eb
vijay vishvkarma <vjvish98@gmail.com>
2022-09-12 00:30:51 +0530

    linkupdate

--- index.php
+++ index.php
@@ -50,16 +50,16 @@ if (isset($_SESSION['first_name'])) {
                 <div class="collapse navbar-collapse text-center" id="collapsenavbar">
                     <ul class="navbar-nav ml-auto line-height ">
                         <li class="nav-item">
-                            <a href="index.php" class="nav-link text-dark">HOME</a>
+                            <a href="" class="nav-link text-dark">HOME</a>
                         </li>
                         <li class="nav-item">
-                            <a href="index.php" class="nav-link text-dark">ABOUT</a>
+                            <a href="" class="nav-link text-dark">ABOUT</a>
                         </li>
                         <li class="nav-item">
                             <a href="index.php" class="nav-link text-dark">SERVICES</a>
                         </li>
                         <li class="nav-item">
-                            <a href="index.php" class="nav-link text-dark">CONTACT</a>
+                            <a href="" class="nav-link text-dark">CONTACT</a>
                         </li>
                         <div class="helpbtn">
                             <a href=<?php echo $menu_btn ?> target=""> <button id="btnID1" onmousedown="changebtn('btnID1')" onmouseup="leavbtn('btnID1')"><i class="fas fa-user-circle fa-lg"></i>&nbsp;&nbsp;<?php echo $menu_btn_text; ?></button></a>
